d1370c61b4ba911915b13c2511571ab57db50ae1
Łukasz Biedrycki <lukasz.biedrycki@gmail.com>
2016-02-19 12:25:54 +0100

    Add ECOVER_NAME variable

--- ecover.mk
+++ ecover.mk
@@ -8,17 +8,29 @@ cover-deinstrument-release:
 
 cover-compile:
 	@echo "Cover compiling release modules"
+ifdef ECOVER_NAME
+	@escript $(ECOVER)/utils/nodetool -name $(ECOVER_NAME) -setcookie $(ECOVER_COOKIE) rpc ecover compile
+else
 	@escript $(ECOVER)/utils/nodetool -sname $(ECOVER_SNAME) -setcookie $(ECOVER_COOKIE) rpc ecover compile
+endif
 
 cover-analyse:
 	@echo "Cover analysing release modules"
+ifdef ECOVER_NAME
+	@escript $(ECOVER)/utils/nodetool -name $(ECOVER_NAME) -setcookie $(ECOVER_COOKIE) rpc ecover analyse
+else
 	@escript $(ECOVER)/utils/nodetool -sname $(ECOVER_SNAME) -setcookie $(ECOVER_COOKIE) rpc ecover analyse
+endif
 	@echo "For acceptance coverage report look at _rel/log/cover/index.html"
 	@echo "To produce unit+acceptance merged code coverage run 'make cover-merge'"
 
 cover-merge:
 	@echo "Merging unit and acceptance tests coverage data"
+ifdef ECOVER_NAME
+	@escript $(ECOVER)/utils/nodetool -name $(ECOVER_NAME) -setcookie $(ECOVER_COOKIE) rpc ecover merge
+else
 	@escript $(ECOVER)/utils/nodetool -sname $(ECOVER_SNAME) -setcookie $(ECOVER_COOKIE) rpc ecover merge
+endif
 	@echo "For total coverage report look at: _rel/log/cover/total/index.html"
 
 cover-check:
